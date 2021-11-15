89c71940161e0f0cee205dc8c946ea2c5ab00ce4
DG-98 <dgarconpro@gmail.com>
2021-11-15 14:03:10 -0500

    Can search games and view game info with API

--- controllers/rawgRoutes.js
+++ controllers/rawgRoutes.js
@@ -7,10 +7,10 @@ router.get("/results", function (req, res) {
 
   axios
     .get(
-      `https://rawg.io/api/games?key=6123ae3d0d994d6aac19947f1736b1fb&search=${gameTitle}`
+      `https://rawg.io/api/games?key=${process.env.RAWG_KEY}&search=${gameTitle}`
     )
     .then((apiResults) => {
-      console.log(apiResults.data.results[3].name)
+      console.log(apiResults.data.results[0].id)
       const results = apiResults.data.results
       res.render("results", { results: results })
     })
@@ -19,6 +19,32 @@ router.get("/results", function (req, res) {
     })
 })
 
+router.get("/:game_id", function(req, res){
+  let rawgId = req.params.game_id 
+
+  axios.get(
+    `https://api.rawg.io/api/games/${rawgId}?key=${process.env.RAWG_KEY}`
+  )
+  .then((apiRes)=>{
+    console.log(apiRes.data.name)
+    let name = apiRes.data.name
+    let description = apiRes.data.description
+    let released = apiRes.data.released 
+    let background_image = apiRes.data.background_image
+    let metacritic = apiRes.data.metacritic
+
+    res.render("detail", {
+      name: name,
+      description: description,
+      released: released,
+      background_image: background_image,
+      metacritic: metacritic,
+    })
+  })
+  .catch((err) => {
+      console.log(err)
+  })
+})
 
 
 
