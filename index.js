9d9f917fc941cf8daae914ab220c20791ebb0804
DG-98 <dgarconpro@gmail.com>
2021-11-22 16:01:09 -0500

    updated db config

--- index.js
+++ index.js
@@ -58,9 +58,6 @@ app.get('/profile', isLoggedIn, (req, res)=>{
 })
 
 
-app.listen(3000, () => {
-  console.log(
-    `process.env.SUPER_SECRET_SECRET ${process.env.SUPER_SECRET_SECRET}`
-  )
-  console.log("auth_practice running on port 3000")
+app.listen(process.env.PORT || 3000, () => {
+  console.log("I hope this works")
 })
